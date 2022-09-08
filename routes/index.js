573eee25e332fc2b568b621de5f894f17543158f
nhadaitran <nhadaitran@gmail.com>
2022-09-08 22:35:01 +0700

    fix deploy vercel

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ const articleRouter = require('../modules/article');
 const categoryRouter = require('../modules/category');
 
 let initWebRouters = (app) => {
-    app.use('', (req, res) => {
-        res.send('Welcome to Revolegue')
-    });
     app.use('/api/v1/user', userRouter);
     app.use('/api/v1/article', articleRouter);
     app.use('/api/v1/category', categoryRouter);
